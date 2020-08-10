162c9502ed1034611a824ac7f17dab869b753d30
chrishargan <chrishargan@gmail.com>
2020-08-10 11:51:09 +0200

    setup for mvc

--- Model/Db_loader.php
+++ Model/Db_loader.php
@@ -6,9 +6,9 @@ class Db_loader
     public function openConnection()
     {
         // No bugs in this function, just use the right credentials.
-        $dbhost = "localhost";
-        $dbuser = "becode";
-        $dbpass = "becode1993";
+        $dbhost = "DB_HOST";
+        $dbuser = "DB_USER";
+        $dbpass = "DB_PASS";
         $db = "crud_db";
 
         $driverOptions = [
