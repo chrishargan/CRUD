5dd2a4c24c1c7e327da7b29081c04309f29cce62
chrishargan <chrishargan@gmail.com>
2020-08-11 12:18:35 +0200

    Attempting to merge project to resolve paths issue

--- View/homepage.php
+++ View/homepage.php
@@ -1,17 +1,37 @@
 <?php
-declare(strict_types=1);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+
 require 'includes/header.php';
 ?>
 <!-- this is the view, try to put only simple if's and loops here.
 Anything complex should be calculated in the model -->
 <section>
-    <h4>Hello <?php ?>,</h4>
+    <form>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Class</label>
+            <input type="text" class="form-control" name="className"  id="exampleFormControlInput1" placeholder="">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Campus</label>
+            <input type="text" class="form-control" name="location"  id="exampleFormControlInput1" placeholder="">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Teacher</label>
+            <input type="text" class="form-control" name="teacherName"  id="exampleFormControlInput1" placeholder="">
+        </div>
+        <input class="btn btn-primary" type="submit" value="Add">
+        <input class="btn btn-primary" type="submit" value="View">
+        <input class="btn btn-primary" type="submit" value="Edit">
+        <input class="btn btn-primary" type="submit" value="Delete">
 
-    <p><a href="View/in?page=info">To info page</a></p>
-
-    <p>Put your content here.</p>
+    </form>
+    <table>
+        <tr><th>Class Name</th><th>Location</th> </tr>
+        <?php
+        foreach($classes as $class=>$value) {
+            echo "<td>{$value['Name']}</td>";
+            echo "<td>{$value['Location']}</td>";
+        }
+        ?>
+    </table>
 </section>
-<?php require 'includes/footer.php'?>
+<?php require 'includes/footer.php' ?>
--- index.php
+++ index.php
@@ -3,55 +3,24 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+// only uncomment for local database password differing from standard becode one
+/*
 define("DB_HOST","localhost");
 define("DB_USER","becode");
-define("DB_PASS","Skeeterpup61!");
+define("DB_PASS","Skeeterpup61!");*/
 
 require 'Controller/Classgroup_Controller.php';
 require 'View/includes/header.php';
 require 'Model/Db_loader.php';
 require 'Model/ClassGroup.php';
 
-//require 'View/homepage.php';
 
 ?>
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-        <title>Who's at BeCode</title>
-    </head>
-    <body>
-    <form>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Class</label>
-            <input type="text" class="form-control" name="className"  id="exampleFormControlInput1" placeholder="">
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Campus</label>
-            <input type="text" class="form-control" name="location"  id="exampleFormControlInput1" placeholder="">
-        </div>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Teacher</label>
-            <input type="text" class="form-control" name="teacherName"  id="exampleFormControlInput1" placeholder="">
-        </div>
-        <input class="btn btn-primary" type="submit" value="Add">
-        <input class="btn btn-primary" type="submit" value="View">
-        <input class="btn btn-primary" type="submit" value="Edit">
-        <input class="btn btn-primary" type="submit" value="Delete">
-
-
-
-
-
 
     <pre>
     <?php
+
         $control = new ClassGroup_controller();
         $control->render($_GET, $_POST);
 
@@ -72,9 +41,7 @@ require 'Model/ClassGroup.php';
 </pre>
 
 
-<hr>
-    </body>
-    </html>
+
 
 
 
